a280c638e3daaace848d12d4f5ebe4b39c7c6d14
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-10-16 09:33:24 +0200

    settings.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+import os
+import django_heroku
+
+# other imports...
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -150,10 +156,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_heroku
-import dj_database_url
-import os
 
 
 django_heroku.settings(locals())
